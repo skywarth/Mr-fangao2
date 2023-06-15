9d3f21e8f207be4cefedc15376db8ce293c19a46
liqifeng <Mr.undefine@protonmail.com>
2023-06-15 11:15:38 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
+<!--
+ * @Author: liqifeng
+ * @Date: 2023-06-15 10:47:04
+ * @LastEditors: liqifeng Mr.undefine@protonmail.com
+ * @LastEditTime: 2023-06-15 11:15:22
+ * @Description: 
+-->
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
@@ -6,6 +13,8 @@
 
 <template>
   <!-- <router-link to="/HelloWorld">To HelloWorld</router-link> -->
+
+  
   <router-view></router-view>
 </template>
 
